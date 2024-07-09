343513ea6f491d8eb6128b24afd62d717bf477bc
Abdulvohis <abdulvohidcode@gmail.com>
2024-07-09 11:54:20 +0300

    first

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+const Home = () => {
+  return (
+    <div className='text-orange-400'>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
