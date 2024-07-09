70a69b6bb4c49407ccc33378f557a04cd8956d70
Abdulvohis <abdulvohidcode@gmail.com>
2024-07-09 14:11:29 +0300

    tailwand

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,42 +1,27 @@
 import React from 'react';
-import uzum from '../assets/uzum.jpg';
+import uzum from '../assets/uzum.png';
+// import { Logo } from '../assets/Assest';
 
 const Header = () => {
   return (
     <div>
-      <div className="header">
-        <div className="left-section">
-          <span className='location'>
-            <i className="fa-solid fa-location-dot location"></i>        
-          </span>
-          <span className='gorod'>Город: </span>
-          <a href="#" className='tashkent'>Ташкент</a>
-          <a href="#" className='punkt'>Пункты выдачи</a>
+        <nav className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <img src="/logo.png" alt="Uzum Market" className="h-8" />
+          <span className="ml-2 text-xl font-bold">uzum market</span>
         </div>
-        <div className="right-section">
-          <a href="#" className='targovliya'>Продавайте на Uzum</a>
-          <a href="#" className='vopros'>Вопрос-ответ</a>
-          <a href="#" className='zakaz'>Мои заказы</a>
-          <img className='flag' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAACGCAMAAACv1DWwAAAAMFBMVEX////VKx4AOKXWLiLv7++dMWfJz+UYNqTfKgoAOKcGO6bh4eH08/G/xdsWOabgLRBiVHJ7AAAA1ElEQVR4nO3ax20DARAEwaV4lDm6/LNVEgJUWHRFMP2fuR8r3OeYFY5CMIVoCtEUoilEU4imEE0hmkI0hWgK0RSiKURTiKYQTSGaQjSFaArRFKJZFPL47wl/4zHPzxWe8/pa4TXnzwrnnB8rFKIpRFOIphBNIZpCNIVoCtEUoilEU4imEE0hmkI0hWgK0RSiKURTiKYQzbnn+XBbYt7fK7znelnhWgimEE0hmkI0hWgK0RSiKUR" alt="" />
-          <a href="#" className="language til">Русский</a>
+        <div className="hidden md:flex space-x-4">
+          <a href="#" className="text-gray-700">Каталог</a>
+          <a href="#" className="text-gray-700">Распродажа</a>
         </div>
-      </div>
-      <div className="catalog-section">
-        <img src={uzum} alt="" className='uzum-logo' />
-        <h2>UZUM MARKET</h2>
-        <div className='Katalog'>
-          
-          <button className='btn-katalog'>
-            <p className='katalog'>Каталог</p>
-          </button>
-          <input type="text" className="search-input" placeholder="Search" />
-          <dir>
-            <a href='#' className=''>войти</a>
-            <a href='#' className=''>избранный</a>
-            <a href='#' className=''>корзина</a>
-          </dir>
+        <div className="flex items-center space-x-4">
+          <a href="#" className="text-gray-700">Войти</a>
+          <a href="#" className="text-gray-700">Избранное</a>
+          <a href="#" className="text-gray-700">Корзина</a>
         </div>
       </div>
+    </nav>
     </div>
   );
 }
---
+++ src/assets/Assest.jsx
@@ -0,0 +1,3 @@
+import Logo from 'https://vectorseek.com/wp-content/uploads/2024/04/Uzum-Market-Logo-Vector.svg-.png';
+
+export const logo = Logo;
